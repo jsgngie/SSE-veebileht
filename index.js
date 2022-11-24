f723ea50917201255bf52755ba6b7efc035b4d46
Hans Eschbaum <Hans.Eschbaum@gmail.com>
2022-11-25 01:52:46 +0200

    Nuppude näitamine/peitmine ja lisa loogika.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 dict = {}
+displayedVars = [];
 
 function addCard(dict) {
     if (document.getElementById("front").value == "" || document.getElementById("back").value == "") {
@@ -11,17 +12,44 @@ function addCard(dict) {
 }
 
 function begin(dict) {
-    var keys = Object.keys(dict);
-    result = keys[Math.floor(keys.length * Math.random())]
-    document.getElementById("card").innerHTML = result;
-    return result
-}
+    if (Object.keys(dict).length === 0) {
+        document.getElementById("card").innerHTML = "Kaarte Pole Lisatud!";
+    } else {
+        result = getRandomKey(dict);
+        document.getElementById("card").innerHTML = result;
+        document.getElementById('alustaNupp').style.visibility = 'hidden';
+        document.getElementById('sisestaNupp').style.visibility = 'hidden';
+        document.getElementById('keeraNupp').removeAttribute("hidden");
 
-function initDict() {
-    var dict = {};
-    return dict;
+        return result
+    }
 }
 
 function flip(dict, result) {
-    document.getElementById("card").innerHTML = dict[result];
+    if (document.getElementById("keeraNupp").innerText == "Keera") {
+        document.getElementById("card").innerHTML = dict[result];
+        displayedVars.push(dict[result])
+        document.getElementById("keeraNupp").innerText = "Järgmine";
+    } else if (document.getElementById("keeraNupp").innerText == "Järgmine") {
+        if (Object.keys(dict).length == displayedVars.length) {
+            document.getElementById("card").innerHTML = "Kaardid on otsas!";
+            document.getElementById("keeraNupp").innerText = "Alusta Uuesti";
+        } else {
+            result = getRandomKey(dict);
+            document.getElementById("card").innerHTML = result;
+            document.getElementById("keeraNupp").innerText = "Keera";
+        }
+    } else if (document.getElementById("keeraNupp").innerText == "Alusta Uuesti") {
+        displayedVars = []
+        result = getRandomKey(dict);
+        document.getElementById("card").innerHTML = result;
+        document.getElementById("keeraNupp").innerText = "Keera";
+    }
+}
+
+function getRandomKey(dict) {
+    keys = Object.keys(dict);
+    availableKeys = keys.filter(n => !displayedVars.includes(n))
+    result = availableKeys[Math.floor(availableKeys.length * Math.random())]
+    return result
 }
